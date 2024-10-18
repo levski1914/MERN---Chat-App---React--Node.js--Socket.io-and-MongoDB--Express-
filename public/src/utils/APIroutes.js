8b6ab3552a10137784d66a197b0c50d6d6b0ed95
Georgi Dimitrov <austria1233@abv.bg>
2024-10-18 20:20:18 +0300

    fix route

--- public/src/utils/APIroutes.js
+++ public/src/utils/APIroutes.js
@@ -1,5 +1,5 @@
 export const host =
-  "mongodb+srv://lamborghinigd:ferrari11@cluster0.zhgjlbl.mongodb.net/";
+  "https://mern-chat-app-react-node-js-socket-io.onrender.com";
 
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
