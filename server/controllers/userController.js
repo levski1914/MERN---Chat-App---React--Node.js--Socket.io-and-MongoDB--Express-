4061011f5eca0cc6b29dad4ac84ec646540c1785
Georgi Dimitrov <austria1233@abv.bg>
2024-10-18 19:33:42 +0300

    change

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 // const { use } = require("../routes/userRoutes");
 module.exports.register = async (req, res, next) => {
   try {
